a608e18fb23c1fa0249504f81f3a268e86a89a0d
chris <Christopherlin3@gmail.com>
2019-10-19 19:52:54 -0700

    Made the main function into a def so it runs faster, also tested video inputs

--- ProjectTest/audioRead.py
+++ ProjectTest/audioRead.py
@@ -2,6 +2,7 @@
 #pip install pydub
 #pip install moviepy
 #install ffmpeg, add to system path
+import math
 
 #these lines import the libraries needed
 import speech_recognition as sr
@@ -9,26 +10,31 @@ from os import path
 from pydub import AudioSegment
 import moviepy.editor as mp
 
-#this takes the first minute of the shia.mp4 video and converts it into test.mp3
-#the clip size can be changed, this is from 0 to 60
-clip = mp.VideoFileClip("shia.mp4").subclip(0,60) 
-clip.audio.write_audiofile("test.mp3")
 
-#this changes the mp3 to wav file
-r = sr.Recognizer()
-src = "test.mp3"
-dst = "test.wav"
-sound = AudioSegment.from_mp3(src)
-sound.export(dst,format="wav")
-
-#the wav file gets recorded
-#audio is now the recorded audio
-harvard = sr.AudioFile('test.wav')
-with harvard as source:
-    audio = r.record(source)
-
-#then it runs the google_recognizer
-#and prints out the lines
-print (r.recognize_google(audio))
+def audio_read(video):
+    #this takes the first minute of the shia.mp4 video and converts it into test.mp3
+    clip = mp.VideoFileClip(video)#.subclip(0,-1)
+    s = math.floor(clip.duration)
 
 
+    clip.audio.write_audiofile("test.mp3")
+
+
+
+    #this changes the mp3 to wav file
+    r = sr.Recognizer()
+    src = "test.mp3"
+    dst = "test.wav"
+    sound = AudioSegment.from_mp3(src)
+    sound.export(dst,format="wav")
+
+    #the wav file gets recorded
+    harvard = sr.AudioFile('test.wav')
+    with harvard as source:
+        audio = r.record(source)
+
+    #then it runs the google_recognizer
+    print (r.recognize_google(audio))
+
+#vid = input("What video would you like to transcribe?")
+audio_read("shia.mp4")
