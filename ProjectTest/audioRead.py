070c161e58a45ab8936358ef81568d5b445d6e39
chris <Christopherlin3@gmail.com>
2019-10-22 10:22:32 -0700

    Split up the video

--- ProjectTest/audioRead.py
+++ ProjectTest/audioRead.py
@@ -14,16 +14,57 @@ import moviepy.editor as mp
 def audio_read(video):
     #this takes the first minute of the shia.mp4 video and converts it into test.mp3
     clip = mp.VideoFileClip(video)#.subclip(0,-1)
+
+
     s = math.floor(clip.duration)
+    sec = math.floor(clip.duration)
+    num = 0
+    num2 = 0
+    s2 = 10
+    
+##    clip2 = clip.subclip(0, s)
+##    clip2.audio.write_audiofile("test" + str(num)+ ".mp3" )
+##
+##    num += 1
+##    clip2 = clip.subclip(40, 50)
+##    clip2.audio.write_audiofile("test" + str(num)+ ".mp3" )
 
+##    s = 60
+##    while s != 0:
+##        #clip = mp.VideoFileClip(video)
+##        #clip2 = clip.subclip(num2, s2)
+##        clip2 = mp.VideoFileClip(video).subclip(num2,s2)
+##        clip2.audio.write_audiofile("test" + str(num)+ ".mp3" )
+##        num += 1
+##        num2 += 10
+##        s2 += 10
+##        s -= 10
+    
+    ##finish up splicing video, is done
+    while s != 0:
+        clip2 = mp.VideoFileClip(video).subclip(num2, s2)
+        clip2.audio.write_audiofile("test" + str(num)+ ".mp3" )
+        num += 1
+        if s > 20:
+            num2 = s2
+            s-=10
+            s2 += 10
+            print(num2)
+            print(s2)
+            print(s)
+        else:
+            num2 = s2
+            s2 += (s-10)
+            print(s2)
+            clip2 = mp.VideoFileClip(video).subclip(num2, s2)
+            clip2.audio.write_audiofile("test" + str(num)+ ".mp3" )
+            s = 0
 
-    clip.audio.write_audiofile("test.mp3")
-
-
+    #TODO read all these videos, lol
 
     #this changes the mp3 to wav file
     r = sr.Recognizer()
-    src = "test.mp3"
+    src = "test5.mp3"
     dst = "test.wav"
     sound = AudioSegment.from_mp3(src)
     sound.export(dst,format="wav")
