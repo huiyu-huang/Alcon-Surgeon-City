cd473c9d7383eee057c46185f0506c6c9c3211a9
Mata17 <kmata@umail.ucsb.edu>
2020-02-03 00:54:53 -0800

    progress

---
+++ front_end_kevin/src/testing/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export const Home =  () => (
+  <div>
+    <h2>Eyelight</h2>
+    <p> This is our senior Capstone Project. We are partnered with Alcon.</p>
+    <p>Also known as True vision. Our goal was to create something that </p>
+    <p> could help surgeons, specifically eye surgeons do their job better </p>
+    <p> we took a post operation approach to the problem and attempted to use </p>
+    <p> machine learing to solve the problem by providing surgeons with a post </p>
+    <p>opertion highlight of the significant parts of the surgery </p>
+  </div>
+
+
+)
