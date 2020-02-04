76deb27b1b5b4c650d1f5e2be23195a946ed20f5
jacobazamora <jacob.zamora524@gmail.com>
2020-02-03 16:16:14 -0800

    Update fbauth.py

--- fbauth.py
+++ fbauth.py
@@ -4,14 +4,14 @@ from flask import *
 app = Flask(__name__)
 config = {
   # confidential
-  "apiKey": "AIzaSyBfJeaQjeU2q_g1zaC_rvJ2D2jEDq58umI",
-  "authDomain": "eyelight-vids.firebaseapp.com",
-  "databaseURL": "https://eyelight-vids.firebaseio.com",
-  "projectId": "eyelight-vids",
-  "storageBucket": "eyelight-vids.appspot.com",
-  "messagingSenderId": "359657414936",
-  "appId": "1:359657414936:web:f24f7ad0acd3a27a2afe6d",
-  "measurementId": "G-RHHY80M6DP"
+  "apiKey": "",
+  "authDomain": "",
+  "databaseURL": "",
+  "projectId": "",
+  "storageBucket": "",
+  "messagingSenderId": "",
+  "appId": "",
+  "measurementId": ""
 }
 
 firebase = pyrebase.initialize_app(config)
