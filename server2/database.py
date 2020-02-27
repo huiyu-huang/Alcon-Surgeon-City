152d3724de58e4a7bd961cb3d0120acafc05d44a
chris <Christopherlin3@gmail.com>
2020-02-26 21:56:40 -0800

    current database

--- server2/database.py
+++ server2/database.py
@@ -35,4 +35,38 @@ def download(video):
     #how to upload file
     with open("static/output.mp4", "wb") as file_obj:
         blob.download_to_file(file_obj)
+        
+def download2(video,identity):
+    global count
+    if  count == 0:
+        cred=credentials.Certificate('acc.json') #credentials, don't know if needed
+        firebase_admin.initialize_app(cred, {
+            'storageBucket': 'meprojec.appspot.com'
+        })
+        count += 1
+    
+    db = firestore.client()
+    bucket = storage.bucket()
+    blob = bucket.blob(video) #what the file name will be called on firebase
+
+    #how to upload file
+    with open("static/" + identity +"/image"+"/toolflow.jpg", "wb+") as file_obj:
+        blob.download_to_file(file_obj)
+        
+def download3(video, identity):
+    global count
+    if  count == 0:
+        cred=credentials.Certificate('acc.json') #credentials, don't know if needed
+        firebase_admin.initialize_app(cred, {
+            'storageBucket': 'meprojec.appspot.com'
+        })
+        count += 1
+    
+    db = firestore.client()
+    bucket = storage.bucket()
+    blob = bucket.blob(video) #what the file name will be called on firebase
+
+    #how to upload file
+    with open("static/" + identity +"/image"+"/pygraph.jpg", "wb+") as file_obj:
+        blob.download_to_file(file_obj)
 
