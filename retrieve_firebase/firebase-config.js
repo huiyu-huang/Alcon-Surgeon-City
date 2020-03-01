0bcb07fee22f2664d7d1a3117fa8a6388ffa170b
jacobazamora <jacob.zamora524@gmail.com>
2020-03-01 10:49:23 -0800

    api key confidential

--- retrieve_firebase/firebase-config.js
+++ retrieve_firebase/firebase-config.js
@@ -1,12 +1,12 @@
 const config = {
-  "apiKey": "AIzaSyBfJeaQjeU2q_g1zaC_rvJ2D2jEDq58umI",
-  "authDomain": "eyelight-vids.firebaseapp.com",
-  "databaseURL": "https://eyelight-vids.firebaseio.com",
-  "projectId": "eyelight-vids",
-  "storageBucket": "eyelight-vids.appspot.com",
-  "messagingSenderId": "359657414936",
-  "appId": "1:359657414936:web:f24f7ad0acd3a27a2afe6d",
-  "measurementId": "G-RHHY80M6DP"
+  "apiKey": "",
+  "authDomain": "",
+  "databaseURL": "",
+  "projectId": "",
+  "storageBucket": "",
+  "messagingSenderId": "",
+  "appId": "",
+  "measurementId": ""
 };
 
 export default config;
