38c4f57f35ef254dc81a3626890f967458599adb
Mata17 <38087289+Mata17@users.noreply.github.com>
2020-03-05 18:18:33 -0800

    added firebase retrieval

--- front_final/src/Watch.js
+++ front_final/src/Watch.js
@@ -5,83 +5,80 @@ import video from './assets/demo_test.mp4'
 import image from './assets/pie_chart.jpg'
 import image1 from './assets/graph.jpg'
 import app from './components/Firebase/base'
+import classAtt from './ClassAtt'
+import axios from "axios";
 
 class Watch extends React.Component{
   // handleSubmit = async (searchTerm) => {
   //   const resonse = await
   // }
   state = {
-    video: '',
-    videoURL: '',
-    progress: 0
+     a: [],
+     got:false
+  };
+  componentDidMount() {
+     this.getUsers();
   }
-
-  handleUploadStart = () => {
-    this.setState({
-      progress: 0
-    })
-  }
-
-  handleUploadSuccess = filename => {
-    this.setState({
-      video: filename,
-      progress: 100
-    })
-    app.storage().ref('eye_vids').child(filename).getDownloadURL()
-      .then(url => this.setState({
-        videoURL: url
-      }))
-  }
-
-  handleProgress = progress => {
-    this.setState({
-      progress: progress
-    })
-  }
+  getUsers = async () => {
+      let res = await axios.get("/watch");
+      let stuff  = res.data;
+      console.log("hi", stuff)
+      console.log("we are here")
+      this.setState({got:true})
+      console.log(this.state.got)
+      this.setState({a:stuff.info})
+      console.log(this.state.a)
+      console.log("now here")
+  };
 
 
   render() {
     return(
       <div>
-        <Grid item xs={12}>
-          <h2>This is your result</h2>
-          <Grid
-            container
-            direction="row"
-            justify="flex-start"
-            alignItems="center"
-          >
-            <Grid item xs={8}>
-              <video width="800" height="550" controls >
-                <source src={video} type="video/mp4"/>
-              </video>
-            </Grid>
-            <Grid item xs={3}>
-              <Grid
-                container
-                direction="column"
-                justify="center"
-                alignItems="flex-end"
-                >
-                  <Grid item xs={6}>
-                    <img src={image} alt="" width="400" height="275" />
-                  </Grid>
-                  <Grid item xs={6}>
-                  <img src={image1} alt="" width="400" height="275" />
-                  </Grid>
+          {this.state.got === false ? (
+              <div>Loading...</div>
+          ) :
+          <div>
+          <Grid item xs={12}>
+            <h2>This is your result</h2>
+            <Grid
+              container
+              direction="row"
+              justify="flex-start"
+              alignItems="center"
+            >
+              <Grid item xs={8}>
+                <video width="800" height="550" controls >
+                  <source src={video} type="video/mp4"/>
+                </video>
               </Grid>
+              <Grid item xs={3}>
+                <Grid
+                  container
+                  direction="column"
+                  justify="center"
+                  alignItems="flex-end"
+                  >
+                    <Grid item xs={6}>
+                      <img src={image} alt="" width="400" height="275" />
+                    </Grid>
+                    <Grid item xs={6}>
+                    <img src={image1} alt="" width="400" height="275" />
+                    </Grid>
+                </Grid>
+              </Grid>
+
             </Grid>
-
           </Grid>
-        </Grid>
 
-        <Grid item xs={8}>
-          <VideoDetail />
-        </Grid>
-        <Grid item xs={4}>
-        </Grid>
+          <Grid item xs={8}>
+            <VideoDetail />
+          </Grid>
+          <Grid item xs={4}>
+          </Grid>
       </div>
-
+    }
+    </div>
   )
   }
 }
